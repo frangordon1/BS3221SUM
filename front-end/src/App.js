f8d5db7fe5d842fae4160e5693514758d36d0b7e
frangordon1 <f.gordon.22@unimail.winchester.ac.uk>
2025-03-19 23:01:37 +0000

    updated wardens

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { getWardens } from './api';
 
 function App() {
-  const [wardens, setWardens] = useState([]);
+
 
   useEffect(() => {
-    getWardens().then(setWardens);
   }, []);
 
-    return (
-        <div>
-            <h1>Warden Tracker</h1>
-            <p>FML I hope this works</p>
-        </div>
-    );
+  return (
+    <div>
+        <h1>Warden Tracker</h1>
+        <p>FML I hope this works</p>
+    </div>
+  );
 }
 
 export default App;
+
 

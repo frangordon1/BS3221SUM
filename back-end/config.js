9cf740ee6a7c1284ea542fcb2d7e689c4c32bf9e
frangordon1 <f.gordon.22@unimail.winchester.ac.uk>
2025-05-22 00:53:24 +0100

    Update config.js

--- back-end/config.js
+++ back-end/config.js
@@ -1,19 +1,16 @@
 const dotenv = require('dotenv');
 
-// Load the .env file if NODE_ENV is set to 'development'
 dotenv.config({ path: './.env.development', debug: true });
 
-// Destructure environment variables
 const {
   AZURE_SQL_USER: user,
   AZURE_SQL_PASSWORD: password,
   AZURE_SQL_SERVER: server,
   AZURE_SQL_DATABASE: database,
   AZURE_SQL_PORT: port,
-  AZURE_SQL_AUTH_TYPE: authenticationType,  // if you have this defined
+  AZURE_SQL_AUTH_TYPE: authenticationType, 
 } = process.env;
 
-// Check if environment variables are loaded correctly
 const passwordConfig = {
   user,
   password,
@@ -25,16 +22,12 @@ const passwordConfig = {
   }
 };
 
-// Debugging log to check values
-console.log("Loaded environment variables:");
-console.log({ server, database, port, authenticationType, user, password });
-
 const noPasswordConfig = {
   server,
   port,
   database,
   authentication: {
-    type: authenticationType,  // e.g., 'sql-login'
+    type: authenticationType, 
     options: {
       userName: user,
       password: password,
